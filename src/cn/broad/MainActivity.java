770532855612e856c9fd1a73c5d7ad89f4c23343
mliwang <1969671488@qq.com>
2016-08-29 10:32:46 +0800

    c

---
+++ src/cn/broad/MainActivity.java
@@ -0,0 +1,30 @@
+package cn.broad;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void click(View v){
+    	Intent intent = new Intent();
+    	intent.setAction("cn.myreceiver");
+    	intent.putExtra("name","我发了个无序广播");
+    	sendBroadcast(intent);//发送无序广播
+    }
+
+}
